10e759006e764e87c7dcb67c65a2bc4c476f6215
Alireza Soheili <71215827+alireza-constantin@users.noreply.github.com>
2022-07-23 15:44:57 +0430

    add navbar styling

---
+++ main.js
@@ -0,0 +1,32 @@
+const primaryHeader = document.querySelector('.primary-header');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-navigation');
+
+function toggleScrolling() {
+	primaryNav.hasAttribute('data-visible')
+		? (document.documentElement.style.overflow = 'hidden')
+		: (document.documentElement.style.overflow = 'auto');
+}
+
+function removeAttribute() {
+	primaryNav.removeAttribute('data-visible');
+	primaryHeader.removeAttribute('data-overlay');
+}
+
+function toggleAttribute() {
+	primaryNav.toggleAttribute('data-visible');
+	primaryHeader.toggleAttribute('data-overlay');
+}
+
+navToggle.addEventListener('click', () => {
+	toggleAttribute();
+	// disabling scrolling
+	toggleScrolling();
+});
+
+primaryHeader.addEventListener('click', (e) => {
+	if (e.target.contains(navToggle)) {
+		removeAttribute();
+		toggleScrolling();
+	}
+});
